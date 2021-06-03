b283c54e677ec807a220220a5af4472de743d517
yejun1060 <yejun1060@daum.net>
2021-06-03 17:19:41 +0900

    Fix migration error

--- main/models.py
+++ main/models.py
@@ -5,7 +5,7 @@ class Account(models.Model):
     id = models.AutoField(primary_key = True)
     clsNum = models.IntegerField()
     name = models.CharField(max_length=5)
-    pw = models.IntegerField
+    pw = models.IntegerField()
 
     def __str__(self):
         return self.name
