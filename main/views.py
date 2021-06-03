399e9f67ddaf8d83663dd738cb6ef9978e0cf39b
yejun1060 <yejun1060@daum.net>
2021-06-04 00:48:56 +0900

    can add value into DB

--- main/views.py
+++ main/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Account
 
 
 def index(request):
-    return HttpResponse("Hello world!")
+    template_name = "index2.html"
+    value = Account.objects.all()
+
+    return render(request, template_name, {"value": value})
+
+
+def insert(request):
+    clsNum = int(request.GET.get('clsNum'))
+    name = request.GET.get('name')
+    pw = 1544
+
+    Account(clsNum=clsNum, name=name, pw=pw).save()
+
+    return index(request)
