632812f392fe6644906427c40ff33d9027ae2602
yejun1060 <yejun1060@daum.net>
2021-06-03 17:16:24 +0900

    Add models and migrate

---
+++ main/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Account(models.Model):
+    id = models.AutoField(primary_key = True)
+    clsNum = models.IntegerField()
+    name = models.CharField(max_length=5)
+    pw = models.IntegerField
+
+    def __str__(self):
+        return self.name
