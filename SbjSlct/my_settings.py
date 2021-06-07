c10da3993ac05e358bca94d945647c17d27de723
yejun1060 <yejun1060@daum.net>
2021-06-07 09:17:54 +0900

    connect to MySQL DB

---
+++ SbjSlct/my_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'admin',
+        'PASSWORD': 'qazx0901',
+        'HOST': 'chiak.cr3vvferpsff.us-east-2.rds.amazonaws.com',
+        'PORT': '3306',
+    }
+}
