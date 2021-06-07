55518f6849b3d4121d67dc299a18cef599b9b261
yejun1060 <yejun1060@daum.net>
2021-06-07 09:59:48 +0900

    add some codes

--- SbjSlct/urls.py
+++ SbjSlct/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('v/', include("main.urls")),
+    path('m/', include("main.urls")),
+    path('',include("index.urls")),
 
 ]
