d03c19ee30fe7498ad29b9305a75628f3c73370d
변예준 (Yejun Byun) <yejun1060@daum.net>
2021-07-01 13:14:28 +0900

    Update views.py
    일부 로직 변경

--- account/views.py
+++ account/views.py
@@ -8,21 +8,26 @@ def login(request):
     if request.method == "POST":
 
         try:
-            if user.objects.get(student_number=request.POST.get("student_number")):
+            # student login
+            if user.objects.filter(student_number=request.POST.get("student_number")):
                 pass
 
+            # student join
             query = user.objects.create(
                 name=request.POST.get("name"),
                 student_number=request.POST.get("student_number"),
+                # homeroom teacher is not assigned
                 homeroom_teacher=teacher.objects.get(id=1),
                 joined_date=t.now(),
                 last_login_date=t.now(),
             ).save()
 
-            return redirect('indexs')
+            # student login logic
+
+            return redirect('index')
 
         except:
-            return render(request, 'html/redirect.html', {})
+            return render(request, 'html/redirect.html', {"error": "로그인 과정 중 오류가 발생한 것 같습니다."})
 
         return redirect('login')
 
