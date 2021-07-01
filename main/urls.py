781ffa20aa5ddbf3716bfe81d7e90ac740b86d2e
변예준 (Yejun Byun) <yejun1060@daum.net>
2021-07-01 13:15:02 +0900

    Update urls.py
    index 호출 로직 일부 변경

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path("2", views.login, name="login"),
-    path("4", views.logout, name='logout'),
-    path("5", views.teacher_login, name='teacher_login'),
+    path("", views.index, name="index"),
 ]
